749e7a39eb9e71c9e78bb1cf15f0d8b06cb5e391
Upendra Dharmagadi <upendratw@gmail.com>
2026-09-14 22:24:53 +0530

    chore: link mobile app to MedicineApp EAS project

--- app.config.ts
+++ app.config.ts
@@ -4,32 +4,43 @@ const config: ExpoConfig = {
   name: 'MedicineApp',
   slug: 'medicineapp-mobile',
   version: '1.0.0',
+
   orientation: 'portrait',
+
   icon: './assets/images/icon.png',
+
   scheme: 'medicineapp',
+
   userInterfaceStyle: 'automatic',
+
   ios: {
     bundleIdentifier: 'com.medicineapp.mobile',
     icon: './assets/expo.icon',
     supportsTablet: true,
   },
+
   android: {
     package: 'com.medicineapp.mobile',
     versionCode: 1,
+
     adaptiveIcon: {
       backgroundColor: '#E8F3F1',
       foregroundImage: './assets/images/android-icon-foreground.png',
       backgroundImage: './assets/images/android-icon-background.png',
       monochromeImage: './assets/images/android-icon-monochrome.png',
     },
+
     predictiveBackGestureEnabled: true,
   },
+
   web: {
     output: 'static',
     favicon: './assets/images/favicon.png',
   },
+
   plugins: [
     'expo-router',
+
     [
       'expo-camera',
       {
@@ -38,8 +49,21 @@ const config: ExpoConfig = {
         recordAudioAndroid: false,
       },
     ],
-    ['expo-secure-store', { configureAndroidBackup: true }],
-    ['expo-notifications', { defaultChannel: 'medicineapp-reminders-v1' }],
+
+    [
+      'expo-secure-store',
+      {
+        configureAndroidBackup: true,
+      },
+    ],
+
+    [
+      'expo-notifications',
+      {
+        defaultChannel: 'medicineapp-reminders-v1',
+      },
+    ],
+
     [
       'expo-splash-screen',
       {
@@ -49,7 +73,18 @@ const config: ExpoConfig = {
       },
     ],
   ],
-  experiments: { typedRoutes: true },
+
+  experiments: {
+    typedRoutes: true,
+  },
+
+  extra: {
+    router: {},
+
+    eas: {
+      projectId: '2f80f2d1-5285-479b-9358-0cf8a88cbe93',
+    },
+  },
 };
 
 export default config;
